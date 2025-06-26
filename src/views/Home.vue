9001fd0410f705f8a64b2e0425cfb1b4c595be4e
ShreyanshvibeC <designsbybaji@gmail.com>
2025-06-26 06:36:07 +0100

    Updated Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -109,7 +109,7 @@
               
               <!-- Temporary Badge -->
               <div v-if="photo.temporary" class="absolute top-4 left-4 bg-gray-900 border border-ncad-green px-3 py-2 z-20">
-                <span class="text-xs font-medium text-ncad-green">Temporary spot</span>
+                <span class="text-xs font-medium text-ncad-green">LEAVING SOON</span>
               </div>
 
               <!-- Visit Count with Gradient -->
