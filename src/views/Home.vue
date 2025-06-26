4afc2534fac50c386a51dd19b0b4a1a744e6bb24
ShreyanshvibeC <designsbybaji@gmail.com>
2025-06-26 04:53:40 +0100

    Updated Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -145,7 +145,7 @@
         @click="handleUploadClick"
         class="fixed bottom-6 left-6 w-20 h-20 bg-black border-2 border-white text-white flex items-center justify-center hover:bg-gray-900 transition-all z-40 shadow-lg"
       >
-        <span class="text-4xl font-light">+</span>
+        <span class="text-4xl font-medium">+</span>
       </button>
     </div>
   </div>
