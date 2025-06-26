5251a82c44a97c70dd55192b63ee3c5f8148c705
ShreyanshvibeC <designsbybaji@gmail.com>
2025-06-26 05:07:38 +0100

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,4 @@
 <script setup lang="ts">
 import HamburgerMenu from './components/HamburgerMenu.vue'
 </script>
-@keyframes rainbow {
-  0% {
-    background-position: 0% 50%;
-  }
-  100% {
-    background-position: 200% 50%;
-  }
-}
 
-.animate-rainbow {
-  animation: rainbow 5s linear infinite;
-}
