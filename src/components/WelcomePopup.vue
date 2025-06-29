a9ba806c5fa668236e241dbac7b287bde47272be
ShreyanshvibeC <designsbybaji@gmail.com>
2025-06-29 15:46:18 +0100

    Updated WelcomePopup.vue

--- src/components/WelcomePopup.vue
+++ src/components/WelcomePopup.vue
@@ -10,7 +10,7 @@
         @click="closePopup"
         class="absolute top-4 right-4 text-white hover:text-gray-400 transition-colors z-10"
       >
-        <span class="text-xl font-bold">✕</span>
+        <span class="text-xl font-900">X</span>
       </button>
       
       <!-- Image at the top -->
