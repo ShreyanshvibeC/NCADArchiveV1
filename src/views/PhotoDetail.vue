f6e4a7a4264dfe220f6b72f23bf925ba6eff812a
ShreyanshvibeC <designsbybaji@gmail.com>
2025-06-30 16:05:13 +0100

    Updated PhotoDetail.vue

--- src/views/PhotoDetail.vue
+++ src/views/PhotoDetail.vue
@@ -78,7 +78,7 @@
                   <div class="absolute top-4 right-4 z-20">
                     <button 
                       @click="showInfoModal = true"
-                      class="w-8 h-8 bg-black border border-white flex items-center justify-center hover:bg-ncad-dark-gray transition-colors"
+                      class="w-8 h-8 bg-black flex items-center justify-center hover:bg-ncad-dark-gray transition-colors"
                     >
                       <svg class="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
